a7f69f5bad1a2aa5f958808c56e7e6cecf9915da
yburkul <yburkul123@gmail.com>
2022-06-10 10:40:27 +0530

    Added:Check the Enter Year is Leap Year or not

---
+++ LeapYearProblem/LeapYearProblem/LeapYear.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearProblem
+{
+    public class LeapYear
+    {
+        public void YearLeap()
+        {
+            int Year;
+            Console.WriteLine("Enter a Year");
+            Year = int.Parse(Console.ReadLine());
+            if (Year > 999)
+            {
+                if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+                {
+                    Console.WriteLine("Entered Year is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("Entered Year is not a Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a Year in Four digit");
+                Console.WriteLine("Enter a Year");
+                Year = int.Parse(Console.ReadLine());
+                if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+                {
+                    Console.WriteLine("Entered Year is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("Entered Year is not a Leap Year");
+                }
+            }
+        }
+    }
+}
---
+++ LeapYearProblem/LeapYearProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeapYearProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LeapYear leapYear = new LeapYear();
+            leapYear.YearLeap();
+        }
+    }
+}
